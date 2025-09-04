c61efce7c1478636f0c2110be7931f15f38b8f29
VaishnaviAgrawa12 <vaishnaviagrawal2999@gmail.com>
2025-09-04 23:32:09 +0530

    feat: added billing service gRPC implementation

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,13 @@
+package com.pm.billingservice.grpc;
+
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest, StreamObserver<billing.BillingResponse> responseStreamObserver){
+
+    }
+}
