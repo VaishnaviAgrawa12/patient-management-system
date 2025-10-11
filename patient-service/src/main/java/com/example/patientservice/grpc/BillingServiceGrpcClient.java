f9ac6b588c8c2d0969d4f0da447f37fff8c5ec20
VaishnaviAgrawa12 <vaishnaviagrawal2999@gmail.com>
2025-10-11 13:04:09 +0530

    billing service grpc

--- patient-service/src/main/java/com/example/patientservice/grpc/BillingServiceGrpcClient.java
+++ patient-service/src/main/java/com/example/patientservice/grpc/BillingServiceGrpcClient.java
@@ -19,7 +19,7 @@ public class BillingServiceGrpcClient {
 
     public BillingServiceGrpcClient(
             @org.springframework.beans.factory.annotation.Value("${billing.service.address:localhost}") String serverAddress,
-            @Value("${billing.service.grpc.port:9001") int serverPort
+            @Value("${billing.service.grpc.port:9001}") int serverPort
 
     ) {
         log.info("Connecting to Billing Service GRPC service at {}:{}",serverAddress,serverPort);
